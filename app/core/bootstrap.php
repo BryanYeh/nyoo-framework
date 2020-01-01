a7e05f122fe136ca920249262bb588e918dac2e2
BryanYeh <bbryanyeh@gmail.com>
2019-12-31 19:25:57 -0800

    fixed namespace issue for linux machines

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -1,13 +1,30 @@
 <?php
 
+// function autoloader($namespace_class)
+// {
+//     $namespace = explode('\\', strtolower($namespace_class));
+//     function () use ($namespace_class) {
+//         var_dump($arg . ' ' . $message);
+//     };
+//     dd($namespace_class,$namespace);
+
+//     $className = ucfirst(array_pop($namespace));
+//     $class = ROOT . DS . implode(DS, $namespace) . DS . $className . '.php';
+
+//     if (file_exists($class)) {
+//         require_once($class);
+//     }
+// }
+
 function autoloader($namespace_class)
 {
-    $namespace = explode('\\', strtolower($namespace_class));
+    $namespace = explode('\\', $namespace_class);
     $className = ucfirst(array_pop($namespace));
-    $class = ROOT . DS . implode(DS, $namespace) . DS . $className . '.php';
+    $class = ROOT . DS . strtolower(implode(DS, $namespace)) . DS . $className . '.php';
 
     if (file_exists($class)) {
         require_once($class);
     }
 }
+
 spl_autoload_register('autoloader');
