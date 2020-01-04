9f6fcb89969fd7db029de0479b6f3e784aea9758
BryanYeh <bbryanyeh@gmail.com>
2020-01-04 13:42:48 -0800

    removed uncessary comment

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -1,21 +1,5 @@
 <?php
 
-// function autoloader($namespace_class)
-// {
-//     $namespace = explode('\\', strtolower($namespace_class));
-//     function () use ($namespace_class) {
-//         var_dump($arg . ' ' . $message);
-//     };
-//     dd($namespace_class,$namespace);
-
-//     $className = ucfirst(array_pop($namespace));
-//     $class = ROOT . DS . implode(DS, $namespace) . DS . $className . '.php';
-
-//     if (file_exists($class)) {
-//         require_once($class);
-//     }
-// }
-
 function autoloader($namespace_class)
 {
     $namespace = explode('\\', $namespace_class);
