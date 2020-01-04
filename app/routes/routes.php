b579921e20c3515552bff48d2e69595652ead53f
BryanYeh <bbryanyeh@gmail.com>
2020-01-04 14:21:21 -0800

    fixed mistake

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-$routes->get('','App\Controllers\HomeController@indedx');
+$routes->get('','App\Controllers\HomeController@index');
 $routes->get('get-info/{id}','App\Controllers\HomeController@database');
 $routes->get('create-info','App\Controllers\HomeController@create');
